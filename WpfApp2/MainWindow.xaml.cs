04507e12f881deff8530174db73cc3516b25ae6d
C3ptV3 <24461272+C3ptV3@users.noreply.github.com>
2020-05-24 20:46:56 +0300

    v1

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -88,8 +88,58 @@ namespace WpfApp2
     }
     public partial class MainWindow : Window
     {
+        public void DoStuff(Grid DynamicGrid)
+        {
+            ColumnDefinition gridCol1 = new ColumnDefinition();
+            gridCol1.Width = new System.Windows.GridLength(60);
+
+            DynamicGrid.ColumnDefinitions.Add(gridCol1);
+        }
+        public void DoStuff1(Grid DynamicGrid)
+        {
+            RowDefinition gridRow1 = new RowDefinition();
+            gridRow1.Height = new System.Windows.GridLength(60);
+            DynamicGrid.RowDefinitions.Add(gridRow1);
+        }
+        public TextBlock DoStuff2(int i, string a)
+        {
+            TextBlock txtBlock1 = new TextBlock();
+
+            txtBlock1.Text = a;
+
+            txtBlock1.FontSize = 15;
+
+            txtBlock1.VerticalAlignment = VerticalAlignment.Top;
+
+            Grid.SetRow(txtBlock1, 0);
+
+            Grid.SetColumn(txtBlock1, i);
+
+            return txtBlock1;
+        }
+        public TextBlock DoStuff3(int i, string a)
+        {
+            TextBlock txtBlock1 = new TextBlock();
+
+            txtBlock1.Text = a;
+
+            txtBlock1.FontSize = 15;
+
+            txtBlock1.VerticalAlignment = VerticalAlignment.Top;
+
+            Grid.SetRow(txtBlock1, i);
+
+            Grid.SetColumn(txtBlock1, 0);
+
+            return txtBlock1;
+        }
         public MainWindow()
         {
+            Grid DynamicGrid = new Grid();
+            DynamicGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            DynamicGrid.VerticalAlignment = VerticalAlignment.Top;
+            DynamicGrid.ShowGridLines = true;
+
             ConsoleManager.Show();
             string refSeq = "ATGCATCCCATGAC";
             string alignSeq = "TCTATATCCGT";
@@ -101,7 +151,42 @@ namespace WpfApp2
             int x = refSeq.Length+1;
             int y = alignSeq.Length+1;
 
+            DynamicGrid.Width = 70 * x;
+            DynamicGrid.Height = 80 * y;
+
+            for (int i = 0; i < x + 2; i++)
+            {
+                DoStuff(DynamicGrid);
+            }
+            for (int i = 0; i < y + 2; i++)
+            {
+                DoStuff1(DynamicGrid);
+            }
+
+            for (int i = 0; i < refSeq.Length; i++)
+            {
+                DynamicGrid.Children.Add(DoStuff2(i + 2, refSeq[i].ToString()));
+
+            }
+            for (int i = 0; i < alignSeq.Length; i++)
+            {
+                DynamicGrid.Children.Add(DoStuff3(i + 2, alignSeq[i].ToString()));
+
+            }
+
             int[,] matrix = new int[y, x];
+
+            int[,] traceback = new int[y, x];
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    traceback[i, j] = 0;
+                }
+
+            }
+
+
             for (int j = 0; j < x; j++)
             {
                 matrix[0, j] = 0;
@@ -110,25 +195,78 @@ namespace WpfApp2
             {
                 matrix[j, 0] = 0;
             }
-            Console.Write("  ");
-            for (int j = 0; j < x-1; j++)
-            {
-                Console.Write(" "+refSeq[j]);
-            }
-            Console.WriteLine();
+            int max=0;
+
+            int[,] maxindex = new int[1, 2];
 
             for (int j = 1; j < y; j++)
             {
-                Console.Write(" " +alignSeq[j-1]);
+                
                 for (int i = 1; i < x; i++)
                 {
                     matrix[j, i] = Math.Max(Math.Max( refSeq[i-1] == alignSeq[j-1] ? (matrix[j-1,i-1]+match) : (matrix[j - 1, i - 1] + mismatch),matrix[j-1,i]+gap), Math.Max(matrix[j, i-1]+gap,0));
-                    Console.Write(" " + matrix[j, i]);
+                    if(matrix[j, i] > max) 
+                    { 
+                        max = matrix[j, i];
+                        maxindex[0, 0] = j;
+                        maxindex[0, 1] = i;
+                    }
                 }
-                Console.WriteLine();
+               
             }
 
+            int lastx = maxindex[0, 0];
+            int lasty = maxindex[0, 1];
 
+            do
+            {
+
+                traceback[lastx, lasty] = 1;
+
+                lastx -= 1;
+                lasty -= 1;
+            } while (matrix[lastx, lasty] != 0);
+
+            traceback[lastx, lasty] = 1;
+            int count = 0;
+            for (int i = 0; i < y; i++)
+                for (int j = 0; j < x; j++)
+                {
+                    {
+                        TextBlock txtBlock1 = new TextBlock();
+                        txtBlock1.Text = string.Empty;
+                        if (traceback[i, j] == 1) { txtBlock1.Background = Brushes.Red;
+                            txtBlock1.Text += "↖";
+                            count += 1;
+                        }
+                        
+                        
+                    
+                        txtBlock1.Text += matrix[i, j].ToString();
+                        txtBlock1.FontSize = 15;
+
+                        txtBlock1.VerticalAlignment = VerticalAlignment.Center;
+
+                        Grid.SetRow(txtBlock1, i + 1);
+
+                        Grid.SetColumn(txtBlock1, j + 1);
+                        DynamicGrid.Children.Add(txtBlock1);
+                    }
+                }
+
+            for(int i = 0; i < count; i++)
+            {
+                Console.Write(refSeq[maxindex[0, 0]-count+i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(refSeq[maxindex[0, 0] - count + i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Score is :"+max);
+
+            Application.Current.MainWindow.Content = DynamicGrid;
         }
     }
 }
